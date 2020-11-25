6298b174d35eb3413a1b868a351a0c15dcafc286
Bhabadyuti <bbal@sii.pl>
2020-11-25 17:09:57 +0530

    empty project

---
+++ src/main/java/com/cleancode/cleancodeapp/CleanCodeAppApplication.java
@@ -0,0 +1,13 @@
+package com.cleancode.cleancodeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCodeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleancode/cleancodeapp/CleanCodeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleancode.cleancodeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCodeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
